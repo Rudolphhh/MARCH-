00ca5ad6f1fb30af2015f3928a03ca49a49c9a32
Rudolphhh <ruda.darilek@gmail.com>
2024-10-19 16:38:58 +0200

    19102024

---
+++ MARCH!/Assets/scripts/Shooter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    public float bulletSpeed = 20f;
+    [SerializeField]
+    private Transform firePoint;
+
+    private float bulletLifeTime = 6f;
+
+    private float reloadTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot", 0f, reloadTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(firePoint.forward * bulletSpeed, ForceMode.Impulse);
+        }
+
+        Destroy(bullet, bulletLifeTime);
+    }
+}
