6dfd2eb0aadb73d5c629d8f95d230236608e9146
Rudolphhh <ruda.darilek@gmail.com>
2024-11-07 11:17:36 +0100

    buttons graphics added

--- MARCH!/Assets/scripts/TrenchLocker.cs
+++ MARCH!/Assets/scripts/TrenchLocker.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrenchLocker : MonoBehaviour
 {
     [SerializeField]
     public bool isLocked = false;
 
-    
+
+    public Image buttonSourceImage;
+    public Sprite unlockedSprite;
+    public Sprite lockedSprite;
+
+
     void Start()
     {
         
@@ -16,7 +22,15 @@ public class TrenchLocker : MonoBehaviour
     
     void Update()
     {
-        
+        if(isLocked)
+        {
+            buttonSourceImage.sprite = lockedSprite;
+        }
+        else
+        {
+            buttonSourceImage.sprite = unlockedSprite;
+        }
+
     }
 
     public void LockTrench()
