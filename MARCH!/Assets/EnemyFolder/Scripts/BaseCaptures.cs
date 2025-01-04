da009a3d1d5b447ac52cb006a173f7d0b9962bcd
Rudolphhh <ruda.darilek@gmail.com>
2025-01-04 09:54:20 +0100

    winscreen, losescreen added

---
+++ MARCH!/Assets/EnemyFolder/Scripts/BaseCaptures.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCaptures : MonoBehaviour
+{
+    public int captures = 3;
+
+
+    public GameObject loseScreen;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("EnemySoldier"))
+        {
+
+            captures--;
+
+            Debug.Log("Zbývající captures: " + captures);
+
+
+            if (captures <= 0)
+            {
+                ShowLoseScreen();
+            }
+        }
+    }
+
+
+    private void ShowLoseScreen()
+    {
+        Debug.Log("Prohra!");
+        if (loseScreen != null)
+        {
+            loseScreen.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("LoseScreen není přiřazen v Inspectoru!");
+        }
+    }
+}
---
+++ MARCH!/Assets/scripts/EnemyBaseCapture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyBaseCapture : MonoBehaviour
+{
+    
+    public int captures = 3;
+
+    
+    public GameObject winScreen;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Soldier"))
+        {
+            
+            captures--;
+
+            Debug.Log("Zbývající captures: " + captures);
+
+            
+            if (captures <= 0)
+            {
+                ShowWinScreen();
+            }
+        }
+    }
+
+    
+    private void ShowWinScreen()
+    {
+        Debug.Log("Vítězství!");
+        if (winScreen != null)
+        {
+            winScreen.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("WinScreen není přiřazen v Inspectoru!");
+        }
+    }
+
+}
