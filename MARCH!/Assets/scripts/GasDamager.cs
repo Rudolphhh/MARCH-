04d7302264af9b7c57920e7eb39caee92ff50a93
Rudolphhh <ruda.darilek@gmail.com>
2025-01-08 21:23:42 +0100

    gasAddedButWillBeChange

---
+++ MARCH!/Assets/scripts/GasDamager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasDamager : MonoBehaviour
+{
+    public int damageAmount = 25;
+    public float damageInterval = 0.5f;
+
+    private HashSet<EnemyHealth> enemiesInGas = new HashSet<EnemyHealth>();
+
+    private void Start()
+    {
+        
+        StartCoroutine(DamageEnemiesOverTime());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemySoldier"))
+        {
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemiesInGas.Add(enemyHealth);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("EnemySoldier"))
+        {
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            if (enemyHealth != null && enemiesInGas.Contains(enemyHealth))
+            {
+                enemiesInGas.Remove(enemyHealth);
+            }
+        }
+    }
+
+    private IEnumerator DamageEnemiesOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(damageInterval);
+
+            foreach (var enemy in enemiesInGas)
+            {
+                if (enemy != null)
+                {
+                    enemy.HP -= damageAmount;
+                }
+            }
+        }
+    }
+}
---
+++ MARCH!/Assets/scripts/GasSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasSpawner : MonoBehaviour
+{
+    public GameObject gasPrefab;
+    public Collider validZone;
+    private bool isSpawningGas = false;
+    public MoneyManager moneyManager;
+
+
+    public void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+    
+    public void EnableGasPlacement()
+    {
+
+        if (moneyManager.money >= 125)
+        {
+            isSpawningGas = true;
+            moneyManager.OdectiPenize(125);
+                
+        }
+
+        
+    }
+
+    private void Update()
+    {
+        if (isSpawningGas && Input.GetMouseButtonDown(0))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                if (hit.collider == validZone)
+                {
+                    SpawnGas(hit.point);
+                    isSpawningGas = false;
+                }
+                
+            }
+        }
+    }
+
+    private void SpawnGas(Vector3 position)
+    {
+        
+        GameObject spawnedGas = Instantiate(gasPrefab, position, Quaternion.identity);
+
+        
+        Destroy(spawnedGas, 10f);
+    }
+
+
+}
