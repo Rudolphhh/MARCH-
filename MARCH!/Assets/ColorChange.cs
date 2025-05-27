c863b25bd09e957bb2302c3074b5d5621f92e1db
Rudolphhh <ruda.darilek@gmail.com>
2025-05-27 15:05:17 +0200

    ButtonsHoverDone

---
+++ MARCH!/Assets/ColorChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TextMeshProUGUI buttonText;
+
+    private Color normalColor;
+    private Color hoverColor;
+
+    private string originalText;
+
+    private void Awake()
+    {
+        ColorUtility.TryParseHtmlString("#323232", out normalColor);
+        ColorUtility.TryParseHtmlString("#444444", out hoverColor);
+
+        if (buttonText != null)
+        {
+            buttonText.color = normalColor;
+            originalText = buttonText.text;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Najetí na tlačítko");
+        if (buttonText != null)
+        {
+            buttonText.color = hoverColor;
+            buttonText.text = "<u>" + originalText + "</u>";
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Opuštění tlačítka");
+        if (buttonText != null)
+        {
+            buttonText.color = normalColor;
+            buttonText.text = originalText;
+        }
+    }
+}
---
+++ MARCH!/Assets/ColorChangeFrontSelect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorChangeFrontSelect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TextMeshProUGUI buttonText;
+
+    private Color normalColor;
+    private Color hoverColor;
+
+    private string originalText;
+
+    private void Awake()
+    {
+        ColorUtility.TryParseHtmlString("#FFFFFF", out normalColor);
+        ColorUtility.TryParseHtmlString("#E5E5E5", out hoverColor);
+
+        if (buttonText != null)
+        {
+            buttonText.color = normalColor;
+            originalText = buttonText.text;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Najetí na tlačítko");
+        if (buttonText != null)
+        {
+            buttonText.color = hoverColor;
+            buttonText.text = "<u>" + originalText + "</u>";
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Opuštění tlačítka");
+        if (buttonText != null)
+        {
+            buttonText.color = normalColor;
+            buttonText.text = originalText;
+        }
+    }
+}
